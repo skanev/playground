9ddfc1637942c133a4963c202d95606b6ba74a6f
Stefan Kanev <stefan.kanev@gmail.com>
2009-05-13 22:28:57 +0300

    Added a glob function

--- haskell/real_world_haskell/Glob.hs
+++ haskell/real_world_haskell/Glob.hs
@@ -1,7 +1,7 @@
 import System.Directory (doesDirectoryExist, doesFileExist, getCurrentDirectory, getDirectoryContents)
---import System.FilePath (dropTrailingPathSeparator, splitFileName, (</>))
-import System.FilePath
+import System.FilePath (dropTrailingPathSeparator, splitFileName, (</>))
 import Control.Monad (forM, filterM, mapM, liftM)
+import Text.Regex.Posix ((=~))
 
 globToRegex :: String -> String
 globToRegex cs = '^' : globToRegex' cs ++ "$"
@@ -40,11 +40,24 @@ lsLa dir = do contents <- (liftM dotless) (getDirectoryContents dir)
               directories <- filterM doesDirectoryExist (map (dir </>) contents)
               nestedFiles <- mapM lsLa directories
               return $ files ++ (concat nestedFiles)
-                 
+
+match :: String -> String -> IO [FilePath]
+match dir glob = case splitFileName glob of
+                    ("", fileGlob) -> do 
+                        result <- getDirectoryContents dir
+                        return $ filter (=~ globToRegex fileGlob) (map (dir </>) (dotless result))
+                    (dirGlob, fileGlob) -> do
+                        matches <- match dir (dropTrailingPathSeparator dirGlob)
+                        dirs <- filterM doesDirectoryExist matches
+                        files <- mapM (`match` fileGlob) dirs
+                        return $ concat files
+
+main = match "../.." "*a*/*haskell"
+{- }
 main = do dir <- getCurrentDirectory
           files <- lsLa dir
           forM files putStrLn
           return ()
-
+-}
 --main = do dir <- getCurrentDirectory
 --         getDirectoryContents dir
