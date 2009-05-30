eef4aa3cb558468e7164cafd6952d067674737f1
Stefan Kanev <stefan.kanev@gmail.com>
2009-05-30 19:59:25 +0300

    Degenerate Objects

--- java/tdd_by_example/Money.java
+++ java/tdd_by_example/Money.java
@@ -10,8 +10,8 @@ class Dollar {
 		this.amount = amount;
 	}
 
-	public void times(int multiplier) {
-		this.amount *= multiplier;
+	public Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
 	}
 
 }
--- java/tdd_by_example/MoneyTest.java
+++ java/tdd_by_example/MoneyTest.java
@@ -4,7 +4,9 @@ import junit.framework.TestCase;
 public class MoneyTest extends TestCase {
 	public void testMultiplication() throws Exception {
 		Dollar five = new Dollar(5);
-		five.times(2);
-		assertEquals(10, five.amount);
+		Dollar product = five.times(2);
+		assertEquals(10, product.amount);
+		product = five.times(3);
+		assertEquals(15, product.amount);
 	}
 }
