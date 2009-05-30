80f613f6242f5b5a9c96fd69611d6e8532fe6a7c
Stefan Kanev <stefan.kanev@gmail.com>
2009-05-30 19:50:56 +0300

    Multi-currency money

--- java/tdd_by_example/Money.java
+++ java/tdd_by_example/Money.java
@@ -2,3 +2,16 @@
 public class Money {
 
 }
+
+class Dollar {
+	int amount;
+	
+	public Dollar(int amount) {
+		this.amount = amount;
+	}
+
+	public void times(int multiplier) {
+		this.amount *= multiplier;
+	}
+
+}
--- java/tdd_by_example/MoneyTest.java
+++ java/tdd_by_example/MoneyTest.java
@@ -2,4 +2,9 @@ import junit.framework.TestCase;
 
 
 public class MoneyTest extends TestCase {
+	public void testMultiplication() throws Exception {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
 }
