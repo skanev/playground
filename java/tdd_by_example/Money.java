5a527430e6a0890d71b9e67be865dc10f3a23bd8
Stefan Kanev <stefan.kanev@gmail.com>
2009-05-30 20:10:21 +0300

    Franc-ly speaking

--- java/tdd_by_example/Money.java
+++ java/tdd_by_example/Money.java
@@ -21,3 +21,22 @@ class Dollar {
 	}
 
 }
+
+class Franc {
+	private int amount;
+	
+	public Franc(int amount) {
+		this.amount = amount;
+	}
+
+	public Franc times(int multiplier) {
+		return new Franc(amount * multiplier);
+	}
+	
+	@Override
+	public boolean equals(Object object) {
+		Franc franc = (Franc) object;
+		return franc.amount == this.amount;
+	}
+
+}
--- java/tdd_by_example/MoneyTest.java
+++ java/tdd_by_example/MoneyTest.java
@@ -2,14 +2,20 @@ import junit.framework.TestCase;
 
 
 public class MoneyTest extends TestCase {
-	public void testMultiplication() throws Exception {
+	public void testMultiplication() {
 		Dollar five = new Dollar(5);
 		assertEquals(new Dollar(10), five.times(2));
 		assertEquals(new Dollar(15), five.times(3));
 	}
 	
-	public void testEquality() throws Exception {
+	public void testEquality() {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
+	}
+	
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
 	}
 }
