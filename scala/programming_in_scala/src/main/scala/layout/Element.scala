4f3acb170a7c1b4a405f598f19e140761633821c
Stefan Kanev <stefan.kanev@gmail.com>
2010-12-01 21:28:49 +0100

    Major revamp of layout.Element
     * Made Element derivatives private
     * Merged the specs for different kinds of elements
     * Reworded the specs

--- scala/programming_in_scala/src/main/scala/layout/Element.scala
+++ scala/programming_in_scala/src/main/scala/layout/Element.scala
@@ -1,13 +1,31 @@
 package layout
 
 object Element {
-  def elem(lines: Array[String]) =
+  private class ArrayElement(
+    val contents: Array[String]
+  ) extends Element
+
+  private class LineElement(line: String) extends Element {
+    def contents = Array(line)
+  }
+
+  private class UniformElement(
+    val fill: Char,
+    override val width: Int,
+    override val height: Int
+  ) extends Element {
+    def contents = Array.make(height, fill.toString * width)
+  }
+
+  def elem(lines: Array[String]): Element =
     new ArrayElement(lines)
-  def elem(line: String) =
+  def elem(line: String): Element =
     new LineElement(line)
-  def elem(fill: Char, width: Int, height: Int) =
+  def elem(fill: Char, width: Int, height: Int): Element =
     new UniformElement(fill, width, height)
 }
+
+import Element.elem
 
 abstract class Element {
   def contents: Array[String]
@@ -16,25 +34,13 @@ abstract class Element {
   override def toString = contents.mkString("\n")
 
   def above(that: Element): Element =
-    new ArrayElement(this.contents ++ that.contents)
+    elem(this.contents ++ that.contents)
 
   def beside(that: Element): Element = {
-    new ArrayElement(
-      for ((line1, line2) <- this.contents zip that.contents) yield line1 + line2
+    elem(
+      for (
+        (line1, line2) <- this.contents zip that.contents
+      ) yield line1 + line2
     )
   }
-}
-
-class ArrayElement(val contents: Array[String]) extends Element
-
-class LineElement(line: String) extends Element {
-  def contents = Array(line)
-}
-
-class UniformElement(
-  val fill: Char,
-  override val width: Int,
-  override val height: Int
-) extends Element {
-  def contents = Array.make(height, fill.toString * width)
 }
--- scala/programming_in_scala/src/test/scala/layout/FactoryMethodsSpec.scala
+++ scala/programming_in_scala/src/test/scala/layout/FactoryMethodsSpec.scala
@@ -5,17 +5,49 @@ import org.scalatest.matchers.ShouldMatchers
 import Element.elem
 
 class FactoryMethodsSpec extends Spec with ShouldMatchers {
-  describe("Factory methods") {
-    it(".elem(lines: Array[String] creates an ArrayElement") {
-      elem(Array("x", "y")).toString should equal ("x\ny")
+  describe("Element.elem(lines: Array[String]") {
+    it("constructs an element from multiple lines of text") {
+      elem(Array("aaa", "bbb", "ccc")).toString should equal ("aaa\nbbb\nccc")
     }
 
-    it(".elem(line: String) creates a LineElement") {
-      elem("line").toString should equal ("line")
+    it("constructs an element as wide as the first string in lines") {
+      elem(Array("12345", "54321")).width should equal (5)
     }
 
-    it(".elem(fill: Char, width: Int, height: Int) creates a UniformElement") {
+    it("constructs an element with width equal to 0 if lines is empty") {
+      elem(Array[String]()).width should equal (0)
+    }
+
+    it("constructs an element with height equal to the length of lines") {
+      elem(Array("1", "2", "3")).height should equal (3)
+    }
+  }
+
+  describe("Element.elem(line: String") {
+    it("constructs a one-row element from line") {
+      elem("12345").toString should equal ("12345")
+    }
+
+    it("constructs an element as wide as line") {
+      elem("12345").width should equal (5)
+    }
+
+    it("constructs an element with height 1") {
+      elem("something").height should equal (1)
+    }
+  }
+
+  describe("Element.elem(fill: Char, width: Int, height: Int)") {
+    it("constructs a width x height block, filled uniformly with fill") {
       elem('o', 3, 2).toString should equal ("ooo\nooo")
+    }
+
+    it("constructs an element with the given width") {
+      elem('o', 3, 2).width should equal (3)
+    }
+
+    it("constructs an element with the given height") {
+      elem('o', 3, 2).height should equal (2)
     }
   }
 }
