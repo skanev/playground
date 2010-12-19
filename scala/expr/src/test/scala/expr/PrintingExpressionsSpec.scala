aeb2c2093b25df87c34a729d1568a4d73b908708
Stefan Kanev <stefan.kanev@gmail.com>
2010-12-19 22:04:22 +0100

    Dried up ExprPrinterSpec

--- scala/expr/src/test/scala/expr/PrintingExpressionsSpec.scala
+++ scala/expr/src/test/scala/expr/PrintingExpressionsSpec.scala
@@ -4,29 +4,16 @@ import org.scalatest.Spec
 import org.scalatest.matchers.ShouldMatchers
 
 class PrintingExpressionsSpec extends Spec with ShouldMatchers {
-  def expectAsStringOf(expr: Expr)(expectation: => String) = {
-    expect(ExprPrinter.asString(expr))(expectation)
-  }
+  val examples = Array(
+    Num(1) + Num(2) -> "1 + 2"
+    , (Name("x") ^ Num(2)) + Num(2) * Name("x") + Num(1) -> "x ^ 2 + 2 * x + 1"
+    , (Name("x") + Num(2)) * Num(3) -> "(x + 2) * 3"
+    , (Num(1) - Num(2)) + (Num(3) - Num(4)) -> "1 - 2 + 3 - 4"
+  )
 
-  it("prints 1 + 2") {
-    val expr = Num(1) + Num(2)
-
-    expectAsStringOf(expr) { "1 + 2" }
-  }
-
-  it("prints x ^ 2 + 2 * x + 1") {
-    val expr = (Name("x") ^ Num(2)) + Num(2) * Name("x") + Num(1)
-    expectAsStringOf(expr) { "x ^ 2 + 2 * x + 1" }
-  }
-
-  it("prints (x + 2) * 3") {
-    val expr = (Name("x") + Num(2)) * Num(3)
-
-    expectAsStringOf(expr) { "(x + 2) * 3" }
-  }
-
-  it("prints 1 - 2 + 3 - 4") {
-    val expr = (Num(1) - Num(2)) + (Num(3) - Num(4))
-    expectAsStringOf(expr) { "1 - 2 + 3 - 4" }
+  for ((expr, string) <- examples) {
+    it("prints " + string) {
+      expect(string) { ExprPrinter.asString(expr) }
+    }
   }
 }
