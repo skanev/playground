913a5ffbd5c69f5d51b05dc6f4f067ee4804b7b6
Stefan Kanev <stefan.kanev@gmail.com>
2010-12-31 14:35:37 +0200

    Cleaned up the property specs

--- scala/expr/src/test/scala/expr/PropertiesSpec.scala
+++ scala/expr/src/test/scala/expr/PropertiesSpec.scala
@@ -4,27 +4,26 @@ import org.scalatest.Spec
 import org.scalatest.prop.Checkers
 import org.scalatest.matchers.ShouldMatchers
 
-import org.scalacheck._
 import org.scalacheck.Prop._
 
 import Expr.parse
 
 class PropertiesSpec extends Spec with Checkers {
-  object ExprProperties extends Properties("expr") {
-    def sameResult(left: Double, right: Double): Boolean = (left == right) || (left.isNaN && right.isNaN)
+  def sameResult(left: Double, right: Double): Boolean = (left == right) || (left.isNaN && right.isNaN)
 
-    property("parse . toString is idemptotent (to some extent)") = forAll(ExprGen.expr) { expr =>
-      expr == parse(expr.toString)
+  describe("An expression") {
+    it("is equal to its parsed toString") {
+      check(forAll(ExprGen.expr) { expr => expr == parse(expr.toString) })
     }
 
-    property("parse . toString evaluates to the same value") = forAll(ExprGen.expr) { expr =>
-      sameResult(expr.eval(ExprGen.env), parse(expr.toString).eval(ExprGen.env))
+    it("is evaluated to the same value as its parse . toString") {
+      check(forAll(ExprGen.expr) { expr =>
+        sameResult(expr.eval(ExprGen.env), parse(expr.toString).eval(ExprGen.env))
+      })
     }
 
-    property("actor evaluation = normal evaluation") = forAll(ExprGen.expr) { expr =>
-      sameResult(expr.eval(ExprGen.env), expr.aeval(ExprGen.env))
+    it("evaluates to the same value with eval() and aeval()") {
+      check(forAll(ExprGen.expr) { expr => sameResult(expr.eval(ExprGen.env), expr.aeval(ExprGen.env)) })
     }
   }
-
-  it("satisfies its properties") { check(ExprProperties) }
 }
