1d6f0174a49612ac90f8726bb01e2805898eab50
Stefan Kanev <stefan.kanev@gmail.com>
2012-03-20 20:34:31 +0200

    sicp; fix borked 1.38

--- scheme/sicp/01/38.scm
+++ scheme/sicp/01/38.scm
@@ -2,8 +2,8 @@
 ;
 ; In 1737, the Swiss mathematician Leonhard Euler published a memoir De
 ; Fractionibus Continuis, which included a continued fraction expansion of e-2,
-; where e is the bas of the natural logarithms. In this fraction, Nᵢ are all 1,
-; and the Dᵢ are successively 1, 2, 1, 1, 4, 1, 1, 6, 1, 1, 8,…. Write a
+; where e is the base of the natural logarithms. In this fraction, Nᵢ are all
+; 1, and the Dᵢ are successively 1, 2, 1, 1, 4, 1, 1, 6, 1, 1, 8,…. Write a
 ; program that uses your cont-frac procedure from exercise 1.37 to approximate
 ; e, based on Euler's expansion.
 
@@ -15,10 +15,11 @@
               (/ (n i) (+ (d i) result)))))
   (iter k 0))
 
-(define (approximate-pi)
+(define (approximate-e)
   (define (term n)
     (if (= (remainder (+ n 1) 3) 0)
         (* 2 (/ (+ n 1) 3))
         1.0))
 
-  (cont-frac (lambda (x) 1.0) term 20))
+  (+ (cont-frac (lambda (x) 1.0) term 20)
+     2))
--- scheme/sicp/01/tests/38-tests.scm
+++ scheme/sicp/01/tests/38-tests.scm
@@ -5,7 +5,7 @@
   (test-suite
     "Tests for SICP exercise 1.38"
 
-    (check-= (approximate-pi) 0.71828183 0.00000001)
+    (check-= (approximate-e) 2.71828183 0.00000001)
 ))
 
 (run-tests sicp-1.38-tests)
