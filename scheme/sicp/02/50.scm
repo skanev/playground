84f2e8b0f9b62735104aea4b7d28ac8bd07a694d
Stefan Kanev <stefan.kanev@gmail.com>
2012-04-19 18:13:25 +0100

    sicp; exercise 2.50 - transformation painters

---
+++ scheme/sicp/02/50.scm
@@ -0,0 +1,23 @@
+; SICP exercise 2.50
+;
+; Define the transformation flip-horiz, which flips painters horizontally, and
+; transformations that rotate painters counterclockwise by 180 degrees and 270
+; degrees.
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
