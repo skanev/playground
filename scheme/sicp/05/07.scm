b98884d6236f5048888ae94a4ce04912561962b3
Stefan Kanev <stefan.kanev@gmail.com>
2012-12-20 23:23:52 +0200

    sicp; exercise 5.07 - simulate 5.04

---
+++ scheme/sicp/05/07.scm
@@ -0,0 +1,7 @@
+; SICP exercise 5.07
+;
+; Use the simulator to test the machines you designed in exercise 5.4.
+
+; How? At this point in the book, we haven't yet completed it.
+;
+; Anyway, I already did in the tests of exercise 5.4
