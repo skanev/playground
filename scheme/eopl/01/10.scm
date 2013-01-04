483945f7494ec6835bd34d6f18275df304a7727b
Stefan Kanev <stefan.kanev@gmail.com>
2013-01-04 15:02:11 +0200

    eopl; exercise 1.10 - the other meaning of "or"

---
+++ scheme/eopl/01/10.scm
@@ -0,0 +1,8 @@
+; EOPL exercise 1.10
+;
+; We typically use "or" to mean "inclusive or". What other meanings can "or"
+; have?
+
+; Of course, it can be "exclusive or", as in the expression "Coffee or tea?".
+; It implies that you have to chose either coffee or tea, but not both. This
+; is known as the XOR operation.
