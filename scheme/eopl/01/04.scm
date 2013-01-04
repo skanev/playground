282c5f0c8611588af727eea9441889f50af48a3c
Stefan Kanev <stefan.kanev@gmail.com>
2013-01-04 14:03:24 +0200

    eopl; exercise 1.04 - List-of-Int derivation

---
+++ scheme/eopl/01/04.scm
@@ -0,0 +1,9 @@
+; EOPL exercise 1.04
+;
+; Write a derivation from List-of-Int to (-7 . (3 . (14 . ())))
+
+;   List-of-Int
+; ⇒ (-7 . List-of-Int)
+; ⇒ (-7 . (3 . List-of-Int))
+; ⇒ (-7 . (3 . (14 . List-of-Int)))
+; ⇒ (-7 . (3 . (14 . ())))
