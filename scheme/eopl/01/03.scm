3ea694a8ab71dc018edd045f857844173e3e35fe
Stefan Kanev <stefan.kanev@gmail.com>
2013-01-04 13:53:32 +0200

    eopl; exericse 1.03 - find a set T != S

---
+++ scheme/eopl/01/03.scm
@@ -0,0 +1,8 @@
+; EOPL exercise 1.03
+;
+; Find a set T of natural numbers such that 0 ∈ T, and whenever n ∈ T, then
+; n + 3 ∈ T, but T ≠ S, where S is the set defined in definition 1.1.2.
+
+; I really don't get the point of this exercise, since it looks simple enough,
+; but if we add that 1 ∈ T, then T would be a subset of S and therefore
+; different than it. Note that it will be the union of S and {n + 1 | n ∈ N}.
