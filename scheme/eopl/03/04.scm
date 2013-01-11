5b142719039a86e0ee60b0e82f2ae6beaebcb9cf
Stefan Kanev <stefan.kanev@gmail.com>
2013-01-11 11:13:46 +0200

    eopl; exercise 3.04 - no idea

---
+++ scheme/eopl/03/04.scm
@@ -0,0 +1,14 @@
+; EOPL exercise 3.04
+;
+; Write out the derivation of figure 3.4 as a derivation tree in the style of
+; one on page 5.
+
+; I cannot figure this exercise out. Figure 3.4 is:
+;
+; (value-of (if-exp exp₁ exp₂ exp₃) p)
+;
+; = (if (expval->bool (value-of exp₁ p))
+;     (value-of exp₂ p)
+;     (value-of exp₃ p))
+;
+; I have no clue how to represent this as a derivation tree. Next!
