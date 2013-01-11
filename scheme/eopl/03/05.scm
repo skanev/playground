a36d7f3f2aba4c315b11f3804db9f77651f5778d
Stefan Kanev <stefan.kanev@gmail.com>
2013-01-11 11:18:18 +0200

    eopl; exercise 3.05 - still no idea

---
+++ scheme/eopl/03/05.scm
@@ -0,0 +1,9 @@
+; EOPL exercise 3.05
+;
+; Write out the derivation of figure 3.5 as a derivation tree in the style of
+; the one on page 5.
+
+; This is the same of the previous and I still can't figure out what I'm
+; supposed to do. I can write a derivation tree, if I had a concrete
+; expression, but it uses exp₁ and exp₂ instead. I can come up with a concrete
+; expression and write the tree, but I just don't think it is worth it.
