84b7fe1de61e01cad3844d1ceb9138c97c15d751
Stefan Kanev <stefan.kanev@gmail.com>
2013-07-11 21:42:59 +0300

    ita; exercise 2.3.2 - merge-sort w/o sentinels

---
+++ other/ita/02/03/02.c
@@ -0,0 +1,35 @@
+void merge(int A[], int p, int q, int r) {
+  int i, j, k;
+
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  int L[n1];
+  int R[n2];
+
+  for (i = 0; i < n1; i++)
+    L[i] = A[p + i];
+  for (j = 0; j < n2; j++)
+    R[j] = A[q + j + 1];
+
+  for(i = 0, j = 0, k = p; k <= r; k++) {
+    if (i == n1) {
+      A[k] = R[j++];
+    } else if (j == n2) {
+      A[k] = L[i];
+    } else if (L[i] <= R[j]) {
+      A[k] = L[i++];
+    } else {
+      A[k] = R[j++];
+    }
+  }
+}
+
+void merge_sort(int A[], int p, int r) {
+  if (p < r) {
+    int q = (p + r) / 2;
+    merge_sort(A, p, q);
+    merge_sort(A, q + 1, r);
+    merge(A, p, q, r);
+  }
+}
