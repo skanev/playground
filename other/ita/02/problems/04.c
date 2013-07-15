87adf2eafe08b9379d105e8ae2784d2776e6e495
Stefan Kanev <stefan.kanev@gmail.com>
2013-07-15 17:21:49 +0300

    ita; problem 2.4 - number of inversions

---
+++ other/ita/02/problems/04.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int merge(int A[], int p, int q, int r) {
+  int i, j, k, inversions = 0;
+
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  int L[n1];
+  int R[n2];
+
+  for (i = 0; i < n1; i++) L[i] = A[p + i];
+  for (j = 0; j < n2; j++) R[j] = A[q + j + 1];
+
+  for(i = 0, j = 0, k = p; k <= r; k++) {
+    if (i == n1) {
+      A[k] = R[j++];
+    } else if (j == n2) {
+      A[k] = L[i++];
+    } else if (L[i] <= R[j]) {
+      A[k] = L[i++];
+    } else {
+      A[k] = R[j++];
+      inversions += n1 - i;
+    }
+  }
+
+  return inversions;
+}
+
+int merge_sort(int A[], int p, int r) {
+  if (p < r) {
+    int inversions = 0;
+    int q = (p + r) / 2;
+    inversions += merge_sort(A, p, q);
+    inversions += merge_sort(A, q + 1, r);
+    inversions += merge(A, p, q, r);
+    return inversions;
+  } else {
+    return 0;
+  }
+}
---
+++ other/ita/02/problems/04.test.c
@@ -0,0 +1,31 @@
+#include "04.c"
+#include "../../build/ext/test.h"
+
+TEST(trivial_case) {
+  int array[] = {1},
+      inversions = merge_sort(array, 0, 1);
+
+  ASSERT_EQUALS(inversions, 0);
+}
+
+TEST(problem_example) {
+  int array[]    = {2, 3, 8, 6, 1},
+      inversions = merge_sort(array, 0, 4);
+
+  ASSERT_EQUALS(inversions, 5);
+}
+
+TEST(chapter_example) {
+  int array[]    = {5, 2, 4, 7, 1, 3, 2, 6},
+      inversions = merge_sort(array, 0, 7);
+
+  ASSERT_EQUALS(inversions, 14);
+}
+
+
+TEST(reversed_array) {
+  int array[]    = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+      inversions = merge_sort(array, 0, 9);
+
+  ASSERT_EQUALS(inversions, 45);
+}
