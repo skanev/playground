8424c903e90293d64aeab3099da68f29d401b333
Stefan Kanev <stefan.kanev@gmail.com>
2013-10-25 11:34:57 +0300

    ita; link to exercises in appendices

--- other/ita/build/lib/renderer.rb
+++ other/ita/build/lib/renderer.rb
@@ -56,8 +56,8 @@ module Renderer
   end
 
   def process(markdown_code)
-    markdown_code = markdown_code.gsub(/exercise\s+(\d+).(\d+)[-.](\d+)/i) { |text| "[#{text}](/%02d/%02d/%02d.html)" % [$1, $2, $3] }
-    markdown_code = markdown_code.gsub(/problem\s+(\d+).(\d+)/i) { |text| "[#{text}](/%02d/%02d.html)" % [$1, $2] }
+    markdown_code = markdown_code.gsub(/exercise\s+([A-D]|\d+).(\d+)[-.](\d+)/i) { |text| "[#{text}](/%s/%02d/%02d.html)" % [ChapterNumber.new($1), $2, $3] }
+    markdown_code = markdown_code.gsub(/problem\s+([A-D]|\d+).(\d+)/i) { |text| "[#{text}](/%s/%02d.html)" % [ChapterNumber.new($1), $2] }
     markdown.render markdown_code
   end
 
