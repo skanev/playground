08e0022aff51526fdd6d6d7dc7e22d1e069edde9
Stefan Kanev <stefan.kanev@gmail.com>
2017-03-26 11:58:14 +0300

    gopl; exercise 1.05 – lissajous green on black

---
+++ go/gopl/01/05/lissajous.go
@@ -0,0 +1,53 @@
+// Exercise 1.5: Change the Lissajous program's color palette to green on
+// black, for added authenticity. To create the web color #RRGGBB, use
+// color.RGBA{0xRR, 0xGG, 0xBB, 0xff} where each pair of hexadecimal digits
+// represents the intensity of the red, green, or blue component of the pixel.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0xFF, 0x00, 0xFF}}
+
+const (
+	foregroundIndex = 0
+	backgroundIndex = 1
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), backgroundIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
