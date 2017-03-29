1393384d5d4a1dc097b7716a4aedb60330d8c296
Stefan Kanev <stefan.kanev@gmail.com>
2017-03-29 07:23:02 +0300

    gopl; exercise 1.08 – fetch with optional protocol

---
+++ go/gopl/01/08/fetch.go
@@ -0,0 +1,33 @@
+// Exercise 1.8: Modify fetch to add the prefix http:// to each argument URL if
+// it is missing. You might want to use strings.HasPrefix.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+			url = "http://" + url
+		}
+
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		_, err = io.Copy(os.Stdout, resp.Body)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
