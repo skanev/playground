085f696281bcd967ad7701208246ef698b68d836
Stefan Kanev <stefan.kanev@gmail.com>
2017-03-29 07:26:11 +0300

    gopl; exercise 1.9 – fetch with status line

---
+++ go/gopl/01/09/fetch.go
@@ -0,0 +1,35 @@
+// Exercise 1.9: Modify fetch to also print the HTTP status code, found in
+// resp.Status
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+			url = "http://" + url
+		}
+
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(resp.Status)
+
+		_, err = io.Copy(os.Stdout, resp.Body)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
